a5f11999021439ffc6d7722a2e97d94a8e6eb220
codekcv <ChristianVillamin31@gmail.com>
2020-11-26 20:33:12 +0800

    fix httpLink

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -27,13 +27,12 @@ const GlobalStyles = createGlobalStyle`
   }
 `;
 
-const httlLink = createHttpLink({
-  // uri: 'https://high-output-wheel.vercel.app/api',
-  uri: 'http://localhost:3000/api',
+const httpLink = createHttpLink({
+  uri: 'https://high-output-wheel.vercel.app/api',
 });
 
 const client = new ApolloClient({
-  link: httlLink,
+  link: httpLink,
   cache: new InMemoryCache(),
 });
 
